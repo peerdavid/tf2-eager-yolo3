273bd80fd56201ad36c92ea215f092b60a95d288
David Peer <d.peer@uibk.ac.at>
2019-10-19 12:06:43 +0200

    Updated video stream

--- video.py
+++ video.py
@@ -28,35 +28,57 @@ argparser.add_argument(
 def main(args):
     """ Show detected objects with boxes, lables and prediction scores in a vide stream
     """
-    # 1. create yolo model & load weights
+    # Load yolo model with pretrained weights
     print("Create YoloV3 model")
     config_parser = ConfigParser(args.config)
     model = config_parser.create_model(skip_detect_layer=False)
     detector = config_parser.create_detector(model)
     
+    # Open video stream
     cap = cv2.VideoCapture(args.camera)
+    if (cap.isOpened()== False): 
+        print("(Error) Could not open video stream")
+        exit()
+
+    # Detect objects in stream
     times = []
+    detect = 0
     while True:
-        _, image = cap.read()
 
-        t1 = time.time()
-        image = image[:,:,::-1]
-        boxes, labels, probs = detector.detect(image, 0.5)
-        t2 = time.time()
-        times.append(t2-t1)
-        times = times[-20:]
+        # Capture every nth frame only because we are too slow 
+        # to capture every frame...
+        ret, image = cap.read()
+        if not ret:
+            print("(Error) Lost connection to video stream")
+            break
 
+        # Detect objects and measure timing
+        if detect <= 0:
+            t1 = time.time()
+            boxes, labels, probs = detector.detect(image, 0.5)
+            t2 = time.time()
+            times.append(t2-t1)
+            times = times[-20:]
+            detect = 50
+        detect -= 1
+
+        # Display detected objects
         visualize_boxes(image, boxes, labels, probs, config_parser.get_labels())
         image = cv2.putText(image, "Time: {:.2f}ms".format(sum(times)/len(times)*1000), (0, 30),
                             cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
-        cv2.imshow('output', image)
-        if cv2.waitKey(1) == ord('q'):
-            break
+        cv2.imshow('Frame', image)
 
+        # Exit with 'q'
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break           
+
+    cap.release()
     cv2.destroyAllWindows()
 
 
-
+#
+# M A I N
+#
 if __name__ == '__main__':
      args = argparser.parse_args()
      main(args)
